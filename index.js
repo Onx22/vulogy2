e467fb8885ceccb36349b98e2116629832ead5a9
Ana Dvt <josueduventru@gmail.com>
2018-08-02 08:51:57 +0200

    login route

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const store = require('./store')
 const app = express()
+
 app.use(express.static('public'))
 app.use(bodyParser.json())
+
+// NEW USER CREATION
+
 app.post('/createUser', (req, res) => {
   store
     .createUser({
@@ -12,6 +16,21 @@ app.post('/createUser', (req, res) => {
     })
     .then(() => res.sendStatus(200))
 })
+// LOGIN
+
+app.post('/login', (req, res) => {
+  store
+    .authenticate({
+      username: req.body.username,
+      password: req.body.password
+    })
+    .then(({ success }) => {
+      if (success) res.sendStatus(200)
+      else res.sendStatus(401)
+    })
+})
+
+// STARTING 
 app.listen(7555, () => {
   console.log('Server running on http://localhost:7555')
 })
