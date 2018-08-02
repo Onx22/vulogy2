b4a4345b8234f002413f87c732753621d254ece7
Ana Dvt <josueduventru@gmail.com>
2018-08-02 09:33:26 +0200

    custom api

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const bodyParser = require('body-parser')
 const store = require('./store')
 const app = express()
 
-app.use(express.static('public'))
+app.use('/public', express.static(__dirname + '/public'))
 app.use(bodyParser.json())
+app.set('view engine', 'ejs');
+
+
+
+app.get('/', (req,res)=>{
+	res.render('loggedpage');
+})
 
 // NEW USER CREATION
 
@@ -25,7 +32,10 @@ app.post('/login', (req, res) => {
       password: req.body.password
     })
     .then(({ success }) => {
-      if (success) res.sendStatus(200)
+      if (success){
+      	// res.sendStatus(200)
+      	res.render("loggedpage")
+      } 
       else res.sendStatus(401)
     })
 })
